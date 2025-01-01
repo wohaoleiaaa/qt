0b736ddd953957062e6a5135ea00251285247bb2
hjx <1526497058@qq.com>
2025-01-02 02:24:20 +0800

    看完part6

--- Lab7/ChatServer/serverworker.cpp
+++ Lab7/ChatServer/serverworker.cpp
@@ -14,6 +14,16 @@ bool ServerWorker::setSocketDescriptor(qintptr socketDescriptor)
     return m_serverSocket->setSocketDescriptor(socketDescriptor);
 }
 
+QString ServerWorker::userName()
+{
+    return m_userName;
+}
+
+void ServerWorker::setUserName(QString user)
+{
+    m_userName = user;
+}
+
 void ServerWorker::onReadyRead()
 {
     QByteArray jsonData;
@@ -24,8 +34,17 @@ void ServerWorker::onReadyRead()
         socketStream.startTransaction();
         socketStream >> jsonData;
         if(socketStream.commitTransaction()){
-            emit logMessage(QString::fromUtf8(jsonData));
-            sendMessage("I recieved message");
+//            emit logMessage(QString::fromUtf8(jsonData));
+//            sendMessage("I recieved message");
+
+            QJsonParseError parseError;
+            const QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData,&parseError);
+            if(parseError.error == QJsonParseError::NoError){
+                if(jsonDoc.isObject()){
+                    emit logMessage(QJsonDocument(jsonDoc).toJson(QJsonDocument::Compact));
+                    emit jsonReceived(this , jsonDoc.object());
+                }
+            }
         }
         else{
             break;
@@ -49,4 +68,14 @@ void ServerWorker::sendMessage(const QString &text, const QString &type)
         //send the JSON using QDataStream
         serverStream << QJsonDocument(message).toJson();
     }
+}
+
+void ServerWorker::sendJson(const QJsonObject &json)
+{
+    const QByteArray jsonData = QJsonDocument(json).toJson(QJsonDocument::Compact);
+    emit logMessage(QLatin1String("Sending to ") + userName() + QLatin1String(" - ") + QString::fromUtf8(jsonData));
+
+    QDataStream socketStream(m_serverSocket);
+    socketStream.setVersion(QDataStream::Qt_5_7);
+    socketStream << jsonData;
 }
